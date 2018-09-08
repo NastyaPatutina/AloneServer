c897c220e40e2388cd1087ff8f359b174d81ed76
Nastya <npatutina@gmail.com>
2018-09-08 18:21:13 +0300

    Добавлен servlet для  приветствия

---
+++ src/main/java/com/hello/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    public static final String START_HTML_FILE="<html><body>";
+    public static final String END_HTML_FILE="</body></html>";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println(START_HTML_FILE + "<h1>Hello World!</h1>" + END_HTML_FILE);
+    }
+
+}
